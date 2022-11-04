e882dfb040eecc20953ff45bf50ec23b7b029f51
Nadi <baykovanadi1958@gmail.com>
2022-11-04 17:43:21 +0300

    2.10 Part 1 Step 2

--- script/script.js
+++ script/script.js
@@ -1,11 +1,9 @@
 'use strict';
 
-var obj = {};
-
-let money = prompt ("Ваш бюджет на месяц?", 0);
+let money = +prompt ("Ваш бюджет на месяц?", 0);
 let time = prompt ("Введите дату в формате YYYY-MM-DD", '');
 
-const appData ={
+const appData = {
     budjet: money,
     timeData: time,
     expenses: {},
@@ -14,12 +12,28 @@ const appData ={
     savings: false
 };
 
-let a1 = prompt ("Введите обязательную статью расходов в этом месяце", '');
-let a2 = prompt ("Во сколько обойдется?", 0);
-let a3 = prompt ("Введите обязательную статью расходов в этом месяце", '');
-let a4 = prompt ("Во сколько обойдется?", 0);
+for (let i = 0; i < 2; i++) {
+    let a = prompt ("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt ("Во сколько обойдется?", 0);
+    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != ''
+        && a.length < 50) {
+            console.log('done');
+            appData.expenses[a] = +b;
+        } else {
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+        }  
+};
 
-alert (appData.budjet / 30);
+appData.moneyPerDay = appData.budjet / 30;
+
+alert("Ежедневный доход: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log('Низкий уровень дохода');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень дохода');
+} else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень дохода');
+} else {
+    console.log('Что-то пошло не так');
+};
